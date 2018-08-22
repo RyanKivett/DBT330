9eb662bacd58cebf522040ab978a952c0a670809
RyanKivett <36863659+RyanKivett@users.noreply.github.com>
2018-08-21 18:39:15 -0600

    MongoService is set up FindOne method is set up

--- Mongo/Program.cs
+++ Mongo/Program.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MongoDB.Bson;
-using MongoDB.Driver;
+using Mongo.Services;
 
 namespace Mongo
 {
@@ -12,43 +11,11 @@ namespace Mongo
     {
         static void Main(string[] args)
         {
-            String connection = "mongodb://localhost";
-            MongoClient client = new MongoClient(connection);
-            var database = client.GetDatabase("Search_Wikipedia");
-            var collection = database.GetCollection<BsonDocument>("Total");
 
-            Console.Write("Enter a title to search for: ");
-            String query = Console.ReadLine();
-
-            FindOne(collection, query);
-
-            //Console.WriteLine(document.Values.ToString());
-
-            //foreach(var document in documents)
-            //{
-            //    Console.WriteLine(document.ToString());
-            //}
+            MongoService mongoService = new MongoService("Search_Wikipedia", "Total");
+            mongoService.FindOne(Console.WriteLine, "Alabama");
         }
 
-        public static void FindOne(IMongoCollection<BsonDocument> collection, string query)
-        {
-            var projection = Builders<BsonDocument>.Projection.Exclude("_id").Include("section_texts");
-
-            var filter = Builders<BsonDocument>.Filter.Eq("title", "Alabama");
-            var document = collection.Find(filter).Project(projection).FirstOrDefault();
-
-            if(document == null)
-            {
-                Console.WriteLine("Article not found");
-            }
-            else
-            {
-                string output;
-                foreach (var value in document.Values)
-                {
-                    Console.WriteLine(value.ToString());
-                }
-            }
-        }
+        
     }
 }
---
+++ Mongo/Services/MongoService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Mongo.Services
+{
+    public class MongoService
+    {
+        public delegate void ReturnDelegate(string output);
+        public string Database { get; set; }
+        public string Collection { get; set; }
+        public MongoService(string database, string collection)
+        {
+            Database = database;
+            Collection = collection;
+        }
+
+        public void FindOne(ReturnDelegate returnDelegate, string query)
+        {
+            String connection = "mongodb://localhost";
+            MongoClient client = new MongoClient(connection);
+            var database = client.GetDatabase(Database);
+            var collection = database.GetCollection<BsonDocument>(Collection);
+
+            var projection = Builders<BsonDocument>.Projection.Exclude("_id").Include("section_texts");
+
+            var filter = Builders<BsonDocument>.Filter.Eq("title", "Alabama");
+            var document = collection.Find(filter).Project(projection).FirstOrDefault();
+
+            if (document == null)
+            {
+                Console.WriteLine("Article not found");
+            }
+            else
+            {
+                string output = "";
+                foreach (var value in document.Values)
+                {
+                    output += value.ToString();
+                }
+
+                output = output.TrimStart(new char[] { '[', '\n' });
+                output = output.TrimEnd(new char[] { ']', '\n' });
+
+                returnDelegate(output);
+            }
+        }
+    }
+}
